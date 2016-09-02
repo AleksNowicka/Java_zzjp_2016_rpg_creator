f632894a4b47fa26a231fdeb6db074cc85107f5e
Karolina Diłaj <Karolina Diaj>
2016-09-02 16:51:29 +0200

    Corrected views in EditExistingGroups and EditExistingCharacters

--- rpg-creator/src/main/java/com/politechnikalodzka/rpgcreator/view/EditExistingCharactersView.java
+++ rpg-creator/src/main/java/com/politechnikalodzka/rpgcreator/view/EditExistingCharactersView.java
@@ -8,21 +8,20 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JEditorPane;
 
 public class EditExistingCharactersView extends JFrame implements FrameSetter{
 
 	public JButton goBackButton;
-
-	private JButton editCharacter1Button;
-	private JButton editCharacter2Button;
-	private JButton editCharacter3Button;
-	private JPanel character1IconPanel;
-	private JPanel character2IconPanel;
-	private JPanel character3IconPanel;
+	private JPanel characterIconPanel;
 	private JLabel frameNameLabel;
-	private JLabel group1Label;
-	private JLabel group2Label;
-	private JLabel group3Label;
 
 	public EditExistingCharactersView(String title) {
 		super(title);
@@ -46,51 +45,35 @@ public class EditExistingCharactersView extends JFrame implements FrameSetter{
 		frameNameLabel.setBounds(100, 11, 260, 25);
 		getContentPane().add(frameNameLabel);
 
-		editCharacter1Button = new JButton("Character 1");
-		editCharacter1Button.setBounds(21, 47, 89, 23);
-		getContentPane().add(editCharacter1Button);
-
-		editCharacter2Button = new JButton("Character 2");
-		editCharacter2Button.setBounds(169, 47, 89, 23);
-		getContentPane().add(editCharacter2Button);
-
-		editCharacter3Button = new JButton("Character 3");
-		editCharacter3Button.setBounds(314, 47, 89, 23);
-		getContentPane().add(editCharacter3Button);
-
-		group1Label = new JLabel("Group 1");
-		group1Label.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		group1Label.setForeground(Color.LIGHT_GRAY);
-		group1Label.setBounds(42, 81, 46, 14);
-		getContentPane().add(group1Label);
-
-		group2Label = new JLabel("Group 2");
-		group2Label.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		group2Label.setForeground(Color.LIGHT_GRAY);
-		group2Label.setBounds(189, 81, 46, 14);
-		getContentPane().add(group2Label);
-
-		group3Label = new JLabel("Group 2");
-		group3Label.setForeground(Color.LIGHT_GRAY);
-		group3Label.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		group3Label.setBounds(339, 81, 46, 14);
-		getContentPane().add(group3Label);
-
-		character1IconPanel = new JPanel();
-		character1IconPanel.setBounds(21, 106, 89, 70);
-		getContentPane().add(character1IconPanel);
-
-		character2IconPanel = new JPanel();
-		character2IconPanel.setBounds(171, 106, 87, 70);
-		getContentPane().add(character2IconPanel);
-
-		character3IconPanel = new JPanel();
-		character3IconPanel.setBounds(314, 106, 89, 70);
-		getContentPane().add(character3IconPanel);
+		characterIconPanel = new JPanel();
+		characterIconPanel.setBounds(21, 48, 89, 125);
+		getContentPane().add(characterIconPanel);
 
 		goBackButton = new JButton("Go back");
 		goBackButton.setBounds(21, 197, 89, 23);
 		getContentPane().add(goBackButton);
+		
+		JComboBox characterComboBox = new JComboBox();
+		characterComboBox.setModel(new DefaultComboBoxModel(new String[] {"Character1", "Character2", "Character3"}));
+		characterComboBox.setBounds(137, 47, 191, 22);
+		getContentPane().add(characterComboBox);
+		
+		JButton btnDeleteCharacter = new JButton("Delete character");
+		btnDeleteCharacter.setBounds(309, 197, 113, 23);
+		getContentPane().add(btnDeleteCharacter);
+		
+		JButton btnSave = new JButton("Save character");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnSave.setBounds(125, 197, 113, 23);
+		getContentPane().add(btnSave);
+		
+		JEditorPane assignGroup = new JEditorPane();
+		assignGroup.setText("Group1");
+		assignGroup.setBounds(137, 88, 106, 20);
+		getContentPane().add(assignGroup);
 	}
 
 	public void setupListeners() {
